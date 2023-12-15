dcd24f9d0e6d96cbfea569b9f478fb6a1330dbd3
Anna <annaocirova5@gmail.com>
2023-12-15 21:11:00 +0800

    Создание контекста БД

---
+++ Infrastructure/DataDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Portfolio.Infrastructure
+{
+    public class DataDbContext : DbContext
+    {
+        public PortfolioContext(DbContextOptions<PortfolioContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().ToTable("Posts");
+            modelBuilder.Entity<Project>().ToTable("Projects");
+            modelBuilder.Entity<Review>().ToTable("Reviews");
+        }
+    }
+}
