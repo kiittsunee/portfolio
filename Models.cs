fef7d63f3c1baf07e5daa3865100e21d5556ebdd
Anna <annaocirova5@gmail.com>
2023-12-15 20:59:02 +0800

    Создание классов моделей

---
+++ Models.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+
+    public class Project
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
